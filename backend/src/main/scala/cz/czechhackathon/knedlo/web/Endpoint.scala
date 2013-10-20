cd2f27273c798d3ba1d1da038b0db995a3d8bda5
Martin Caslavsky <mcaslavsky@gmail.com>
2013-10-20 11:32:56 +0200

    chg: handle dummy user

--- backend/src/main/scala/cz/czechhackathon/knedlo/web/Endpoint.scala
+++ backend/src/main/scala/cz/czechhackathon/knedlo/web/Endpoint.scala
@@ -9,7 +9,6 @@ import javax.annotation.Nullable
 import cz.czechhackathon.knedlo.util.Logging
 import cz.czechhackathon.knedlo.service.{UserService, FeedService}
 import cz.czechhackathon.knedlo.dao.{UserDao, CategoryDao}
-import com.google.appengine.api.oauth.OAuthRequestException
 
 @Api(name = "knedlo", version = "v1", clientIds = Array(
   "338711060290.apps.googleusercontent.com", // iOS
@@ -19,62 +18,53 @@ import com.google.appengine.api.oauth.OAuthRequestException
 )
 class Endpoint extends Logging {
 
-  val userDao = new UserDao
-  val categoryDao = new CategoryDao
-  val userService = new UserService(userDao)
-  val feedService = new FeedService(userDao = userDao, categoryDao = categoryDao)
+  private val userDao = new UserDao
+  private val categoryDao = new CategoryDao
+  private val userService = new UserService(userDao)
+  private val feedService = new FeedService(userDao = userDao, categoryDao = categoryDao)
+  private val dummy = new User("test@gmail.com", "testDomain")
 
   /**
    * Get paged article feed for the given user
    * @param page page number (first page if not entered)
-   * @param user user
+   * @param u user
    * @return list of articles or empty list
    */
   @ApiMethod(name = "feed", path = "feed", httpMethod = HttpMethod.GET)
-  def feed(@Named("page") @Nullable page: Integer, user: User): Array[Article] = {
-    if (user != null) {
-      log.info(s"${user.toString} - email: ${user.getEmail}") // TODO remove
-      userService.saveIfMissing(user) // TODO tady? a kdy a jak mu pridat clanky?
-      feedService.getFeed(user.getEmail)
-    } else {
-      //TODO throw new OAuthRequestException("Unauthorized")
-      log.info(s"user is null")
-      val testEmail = "test@gmail.com"
-      userService.saveIfMissing(new User(testEmail, "testDomain"))
-      // TODO onboarding (fill user's feed)
-      feedService.getFeed(testEmail)
-    }
+  def feed(@Named("page") @Nullable page: Integer, u: User): Array[Article] = {
+    //TODO throw new OAuthRequestException("Unauthorized")
+    val user = if (u != null) u else dummy
+    log.info(s"${user.toString} - email: ${user.getEmail}")
+    userService.saveIfMissing(user) // TODO tady? a kdy a jak mu pridat clanky?
+    // TODO onboarding (fill user's feed)
+    feedService.getFeed(user.getEmail)
   }
 
   /**
    * Record client action
    * @param action action id (read, save, ignore,...)
    * @param articleLink link of affected article
-   * @param user user
+   * @param u user
    * @return list of newly awarded badges or empty list
    */
   @ApiMethod(name = "action", path = "action", httpMethod = HttpMethod.POST)
-  def action(@Named("action") action: String, @Named("articleLink") articleLink: String, user: User):Array[Badge] = {
-    if (user != null) {
-      feedService.action(action, articleLink, user.getEmail)
-    } else {
-      //TODO throw new OAuthRequestException("Unauthorized")
-      feedService.action(action, articleLink, "test@gmail.com")
-      Array(new Badge("Stará bačkora", "Přečetl jsi recenze 10 černobílých filmů", "http://www.nadmerneboty.cz/fotky4936/fotos/_vyr_266BP0773.jpg"))
-    }
+  def action(@Named("action") action: String, @Named("articleLink") articleLink: String, u: User):Array[Badge] = {
+    //TODO throw new OAuthRequestException("Unauthorized")
+    val user = if (u != null) u else dummy
+    log.info(s"${user.toString} - email: ${user.getEmail}")
+    feedService.action(action, articleLink, user.getEmail)
   }
 
   /**
    * Get badges for the given user
-   * @param user user
+   * @param u user
    * @return list of badges or empty list
    */
   @ApiMethod(name = "badges", path = "badges", httpMethod = HttpMethod.GET)
-  def badges(user: User): Array[Badge] = {
-    if (user != null) {
-      categoryDao.find(user.getEmail)
-    } else {
-      throw new OAuthRequestException("Unauthorized")
-    }
+  def badges(u: User): Array[Badge] = {
+    //TODO throw new OAuthRequestException("Unauthorized")
+    val user = if (u != null) u else dummy
+    log.info(s"${user.toString} - email: ${user.getEmail}")
+    categoryDao.find(user.getEmail)
   }
 }
